d1dceb6de9b0935730d872a094d290db18669f45
lisaco-ono <lisaco.ono.ii@gmail.com>
2018-08-01 20:17:55 +0900

    first commit: can calculate under 9

--- reviewSwift/ViewController.swift
+++ reviewSwift/ViewController.swift
@@ -8,11 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
+    var firstNum = 0.0
+    var secondNum = 0.0
+    var whichCalc = 0.0
+
+    @IBOutlet weak var NumLabel: UILabel!
+    @IBAction func NumButton(_ sender: UIButton) {
+        firstNum = secondNum
+        secondNum = Double(sender.tag)
+        NumLabel.text = secondNum.description
+    }
+    @IBAction func WhichCalc(_ sender: UIButton) {
+        whichCalc = Double(sender.tag)
+        firstNum = secondNum
+    }
+    @IBAction func EqualButton(_ sender: Any) {
+        switch whichCalc {
+        case 1:
+            firstNum = firstNum + secondNum
+            break
+        case 2:
+            firstNum = firstNum - secondNum
+            break
+        case 3:
+            firstNum = firstNum * secondNum
+            break
+        case 4:
+            firstNum = firstNum / secondNum
+            break
+        default:
+            print("Error")
+        }
+        secondNum = firstNum
+        NumLabel.text = firstNum.description
+    }
+    @IBAction func ACButton(_ sender: Any) {
+        firstNum = 0
+        secondNum = 0
+        NumLabel.text = firstNum.description
+    }
+    @IBAction func PlusOrMinus(_ sender: Any) {
+        secondNum *= -1
+        NumLabel.text = secondNum.description
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        firstNum = 0.0
+        secondNum = 0.0
+        whichCalc = 0.0
+        
     }
 
     override func didReceiveMemoryWarning() {
